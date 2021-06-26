4aaa09655d01c461765659cc6ee6f7e797540403
Anatolii Veselyi <anatoliiv1990@gmail.com>
2021-06-26 20:47:44 +0300

    templates and new BP

--- my_blog/auth.py
+++ my_blog/auth.py
@@ -4,7 +4,7 @@ from flask import (
     Blueprint, g, request, redirect, flash, render_template, url_for, session
 )
 from werkzeug.security import generate_password_hash, check_password_hash
-from my_blog import get_db
+from my_blog.db import get_db
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -20,23 +20,23 @@ def register():
         if not username:
             error = 'Username is required'
         elif not password:
-            error = 'Password is reauired'
+            error = 'Password is required'
         elif db.execute(
             'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
             error = f'User {username} is already registered.'
 
-            if error is None:
-                db.execute(
-                    'INSERT INTO user (username, password) VALUES (?, ?)', 
-                    (username,generate_password_hash(password))
-                )
-                db.commit()
-                return redirect(url_for('auth.login'))
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)', 
+                (username,generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
 
-            flash(error)
+        flash(error)
 
-        return render_template('auth/register.html')
+    return render_template('auth/register.html')
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
